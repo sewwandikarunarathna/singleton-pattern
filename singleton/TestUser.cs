1e79c1598406dcfd36849a27eec0ec8df3c27626
sewwandi karunarathna <sewwandi1104@gmail.com>
2021-01-08 22:31:59 +0530

    first commit

---
+++ singleton/TestUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+	class TestUser
+	{
+		static void Main(string[] args)
+		{
+			User obj1 = User.GetInstance();
+			User obj2 = User.GetInstance();
+
+			Console.WriteLine(obj1.GetHashCode());
+			Console.WriteLine(obj2.GetHashCode());
+		}
+	}
+}
---
+++ singleton/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    class User
+    {
+        private static User user;
+
+        private User()
+        {
+
+        }
+
+        public static User GetInstance()
+        {
+            if (user == null)
+            {
+                user = new User();
+                return user;
+            }
+            return user;
+        }
+
+    }
+}
